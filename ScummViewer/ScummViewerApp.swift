5a01d0a18423dd4793df9244863272d6ef12ed1c
Michael Borgmann <michaelborgmann@mailbox.org>
2021-10-13 09:59:16 +0100

    Xcode base project for SwiftUI on macOS

---
+++ ScummViewer/ScummViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScummViewerApp.swift
+//  ScummViewer
+//
+//  Created by Michael Borgmann on 13.10.21.
+//
+
+import SwiftUI
+
+@main
+struct ScummViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
